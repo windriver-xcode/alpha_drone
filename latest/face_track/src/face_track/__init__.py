bcb417e21e7a418e0011dc033c7163c03094ccd0
shijq23 <shijq23@users.noreply.github.com>
2021-06-25 17:21:31 -0400

    removed duplicate files

--- latest/face_track/src/face_track/__init__.py
+++
@@ -1,3 +0,0 @@
-from face_track.mockdjitellopy import Tello
-from face_track.pid import PID
-from face_track.tracker import FaceTracker
--- latest/tests/test_sample.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-def test_m1():
-    pass
-
-def test_m2():
-    pass
